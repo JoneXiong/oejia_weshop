b81b2c7601141dc4f3cab9732b80a85a7ff1b065
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2018-09-21 18:20:01 +0800

    修复odoo默认对application/json的请求强制做request内容json校验

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from types import MethodType
+
 from . import base
 from . import config
 from . import banner
@@ -8,3 +10,19 @@ from . import product
 from . import user
 from . import address
 from . import order
+
+
+from odoo.http import root, JsonRequest, HttpRequest
+
+
+def get_request(self, httprequest):
+    if 'MicroMessenger' in httprequest.headers['User-Agent']:
+        return HttpRequest(httprequest)
+    if httprequest.args.get('jsonp'):
+        return JsonRequest(httprequest)
+    if httprequest.mimetype in ("application/json", "application/json-rpc"):
+        return JsonRequest(httprequest)
+    else:
+        return HttpRequest(httprequest)
+
+root.get_request = MethodType(get_request, root)
