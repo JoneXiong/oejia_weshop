c45b9814b33a9df57b417325c953590cb961f5fb
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2020-03-04 16:38:03 +0800

    修复html内容换行无效的问题

--- controllers/base.py
+++ controllers/base.py
@@ -148,7 +148,6 @@ class BaseController(object):
 
 def convert_static_link(request, html):
     base_url = request.env['ir.config_parameter'].sudo().get_param('web.base.url')
-    html = html.replace('<p>', '').replace('</p>', '')
     return html.replace('src="', 'src="{base_url}'.format(base_url=base_url))
 
 
