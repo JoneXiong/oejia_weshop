7d8ded7f3c59673e6bb7e308c2d0ec357f7279fe
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2022-08-08 19:45:46 +0800

    省市区名称必填

--- models/oe_city.py
+++ models/oe_city.py
@@ -9,7 +9,7 @@ class City(models.Model):
     _description = u'城市'
 
     pid = fields.Many2one('oe.province', string='省份')
-    name = fields.Char('名称')
+    name = fields.Char('名称', requried=True)
     child_ids = fields.One2many('oe.district', 'pid', string='区')
 
 
